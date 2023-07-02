8668bb50a48985df80e1c3041215e9ad2714b055
Blaise <ejikemeblaise08@gmail.com>
2023-07-02 23:33:00 +0100

    modified challange29

--- Solutions/challange29.js
+++ Solutions/challange29.js
@@ -1,27 +1,39 @@
-//My solution 
 const slotMachine = document.querySelector('.emoji-slots-game'); 
 const food = 'https://apis.scrimba.com/emojihub/api/all/category/food-and-drink';
 
 function makeFruitArray(arr){
-   return arr.filter(food => {
-        return food.group === `food fruit`
-   })
+   // filter by group prop, looking for a str that includes the word "fruit"
+   return arr.filter(emoji => emoji.group.includes("fruit"));
 }
 
-function getRandomFruits(){
-    fetch(food)
-         .then(response => response.json())
-         .then(result => makeFruitArray(result)) 
-         .then(data => {
-            console.log(data)
-            data.forEach(({htmlCode}) => {
-                const random = Math.floor(Math.random() * data.length)
-                 return slotMachine.innerHTML += ` ${htmlCode}`
-            })
-           
-        })
-        .catch(err => err)
+function getRandomFruits(arr){
+    // initialize a new arr to hold 9 random fruits
+    const randomFruits = [];
+    // create a loop 0 - 9
+    for(let i = 0; i < 9; i++){
+        // generate a random num
+        // use the random number as an index to choose a random fruit from the fruit arr
+        // push the random fruit into new arr 
+        let randNum = Math.floor(Math.random() * arr.length);
+        randomFruits.push(arr[randNum]);
+    }
+    console.log(randomFruits.length)
+    return randomFruits
 }
 
- getRandomFruits()
 // write your fetch request here 
+fetch(food)
+    .then(response => response.json())
+    .then(result => makeFruitArray(result))
+    .then(fruits => getRandomFruits(fruits))
+    .then(fruit => {
+        fruit.forEach(fruit => {
+            console.log(fruit.htmlCode)
+            slotMachine.innerHTML += `
+                <li>
+                    ${fruit.htmlCode}
+                </li>
+            `
+        })
+    })
+    .catch(err => console.log(err))
